3e45566d7cc450e600b52ccd2d07d0d5a289f870
CodeBloodedMama <84911345+CodeBloodedMama@users.noreply.github.com>
2022-11-08 11:17:04 +0100

    IranAirline metoder done

--- SD_Assign_Mediator_Pattern_Design/Airlines/IranAirline.cs
+++ SD_Assign_Mediator_Pattern_Design/Airlines/IranAirline.cs
@@ -9,6 +9,45 @@ namespace SD_Assign_Mediator_Pattern_Design.Airlines
 {
     public class IranAirline : AbstractAirLine
     {
+        public string[] Departures = { "Copenhagen", "London", "Paris" };
+        public string[] Arrivals = { "Tehran", "Dubai", "Alanya" };
+        public string NameOfAirline = "IranAirline";
+        public double Rate = 5.0;
+
+        public int CalculatePriceOfRoute(string departure, string arrival, int passengers)
+        {
+            if (departure == "Copenhagen" && arrival == "Tehran")
+            {
+                this.Price = passengers * 100;
+                return Price;
+            }
+
+            return 0;
+        }
+
+        public int CalculateDurationOfRoute(string departure, string arrival)
+        {
+            if (departure == "Copenhagen" && arrival == "Tehran")
+            {
+                this.Duration = 3;
+                return Duration;
+            }
+
+            return 0;
+        }
+
+        public int CalculateStopsOfRoute(string departure, string arrival)
+        {
+            if (departure == "Copenhagen" && arrival == "Tehran")
+            {
+                return this.stops = 3;
+            }
+
+            return 0;
+        }
+
+        
+
         public override void CancelFlight()
         {
             Console.WriteLine("Iran Airline has cancelled the flight");
