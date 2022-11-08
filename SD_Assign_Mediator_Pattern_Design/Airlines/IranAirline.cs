424647de6511dd5be63c552fb73228ab881fe6fb
au618503 <58029607+au618503@users.noreply.github.com>
2022-11-08 10:13:05 +0100

    Added to Iran and KLM airlines

--- SD_Assign_Mediator_Pattern_Design/Airlines/IranAirline.cs
+++ SD_Assign_Mediator_Pattern_Design/Airlines/IranAirline.cs
@@ -9,6 +9,43 @@ namespace SD_Assign_Mediator_Pattern_Design.Airlines
 {
     public class IranAirline : AbstractAirLine
     {
+        public string[] Departures = { "Copenhagen", "London", "Paris", "Croatia" };
+        public string[] Arrivals = { "Turkiye", "Hamburg", "USA", "Frankfurt" };
+        public string NameOfAirline = "Iran Airline";
+        //public double Rate = ;
+
+        public int CalculatePriceOfRoute(string departure, string arrival, int passengers)
+        {
+            if (departure == "Copenhagen" && arrival == "Hamburg")
+            {
+                this.Price = passengers * 150;
+                return Price;
+            }
+
+            return 0;
+        }
+
+        public int CalculateDurationOfRoute(string departure, string arrival)
+        {
+            if (departure == "Copenhagen" && arrival == "Hamburg")
+            {
+                this.Duration = 3;
+                return Duration;
+            }
+
+            return 0;
+        }
+
+        public int CalculateStopsOfRoute(string departure, string arrival)
+        {
+            if (departure == "Copenhagen" && arrival == "Hamburg")
+            {
+                return this.stops = 2;
+            }
+
+            return 0;
+        }
+
         public override void CancelFlight()
         {
             Console.WriteLine("Iran Airline has cancelled the flight");
--- SD_Assign_Mediator_Pattern_Design/Airlines/KLMAirline.cs
+++ SD_Assign_Mediator_Pattern_Design/Airlines/KLMAirline.cs
@@ -8,6 +8,44 @@ namespace SD_Assign_Mediator_Pattern_Design.Airlines
 {
     public class KLMAirline : AbstractAirLine
     {
+        public string[] Departures = { "Copenhagen", "London", "Paris", "Crotia", "India", "Malaga" };
+        public string[] Arrivals = { "Turkiye", "Hamburg", "USA", "Frankfurt", "Billund", "Barcelona" };
+        public string NameOfAirline = "KLM Airline";
+        //public double Rate = ;
+
+        public int CalculatePriceOfRoute(string departure, string arrival, int passengers)
+        {
+            if (departure == "Copenhagen" && arrival == "Hamburg")
+            {
+                this.Price = passengers * 225;
+                return Price;
+            }
+
+            return 0;
+        }
+
+        public int CalculateDurationOfRoute(string departure, string arrival)
+        {
+            if (departure == "Copenhagen" && arrival == "Hamburg")
+            {
+                this.Duration = 1;
+                return Duration;
+            }
+
+            return 0;
+        }
+
+        public int CalculateStopsOfRoute(string departure, string arrival)
+        {
+            if (departure == "Copenhagen" && arrival == "Hamburg")
+            {
+                this.stops = 1;
+                return stops;
+            }
+
+            return 0;
+        }
+
         public override void CancelFlight()
         {
             Console.WriteLine("KLM Airline has cancelled the flight");
